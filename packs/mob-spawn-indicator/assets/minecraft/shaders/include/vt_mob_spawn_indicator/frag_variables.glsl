ce0d077e2b75e93ab01999864cd4608c99e0f09d
hatrix1307 <sonicthehotdog78@outlook.com>
2026-03-09 00:37:46 -0400

    replaced pax

--- packs/mob-spawn-indicator/assets/minecraft/shaders/include/vt_mob_spawn_indicator/frag_variables.glsl
+++
@@ -1,7 +0,0 @@
-in vec2 lightLevel;
-in vec2 faceCoords;
-in float stairs;
-
-#define THICKNESS (sqrt(2.0) / 2.0 / 16.0)
-#define OUTLINE ((16.0 - (7.5 * sqrt(2.0))) / 32.0)
-#define CORNER (9.9 / 16.0)
